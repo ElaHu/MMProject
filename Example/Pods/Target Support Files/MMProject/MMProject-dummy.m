d8ce77277cd4c2704add12d432d931b03d315993
huweihong <huweihong@miao.cn>
2019-04-18 13:34:35 +0800

    init

---
+++ Example/Pods/Target Support Files/MMProject/MMProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MMProject : NSObject
+@end
+@implementation PodsDummy_MMProject
+@end
---
+++ MMProject/Classes/aaa.m
@@ -0,0 +1,15 @@
+//
+//  aaa.m
+//  TTProject
+//
+//  Created by huweihong on 2019/4/9.
+//  Copyright © 2019年 huweihong. All rights reserved.
+//
+
+#import "aaa.h"
+
+@implementation aaa
++ (void)load{
+    NSLog(@"aaa走了Load");
+}
+@end
